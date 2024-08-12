5881bdba5f9b68801c110323ca8e53939fcb71d1
ngocthanhdoan <55902150+ngocthanhdoan@users.noreply.github.com>
2024-08-12 10:02:54 +0700

    update get env

--- src/main/java/com/manager/web/app/config/DataSetConfig.java
+++ src/main/java/com/manager/web/app/config/DataSetConfig.java
@@ -1,27 +1,49 @@
 package com.manager.web.app.config;
 
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
 import org.sql2o.Sql2o;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Component
 public class DataSetConfig {
 
-    @Value("${spring.datasource.url}")
-    private String dbUrl;
+    private final Environment env;
 
-    @Value("${spring.datasource.username}")
-    private String dbUsername;
-
-    @Value("${spring.datasource.password}")
-    private String dbPassword;
-
-    @Value("${spring.datasource.driver-class-name}")
-    private String dbDriverClassName;
+    public DataSetConfig(Environment env) {
+        this.env = env;
+    }
 
     @Bean
     public Sql2o sql2o() {
+        String dbUrl = env.getProperty("spring.datasource.url");
+        String dbUsername = env.getProperty("spring.datasource.username");
+        String dbPassword = env.getProperty("spring.datasource.password");
         return new Sql2o(dbUrl, dbUsername, dbPassword);
     }
 }
+
+// import org.sql2o.Sql2o;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+
+// @Configuration
+// public class DataSetConfig {
+
+//     @Value("${spring.datasource.url}")
+//     private String dbUrl;
+
+//     @Value("${spring.datasource.username}")
+//     private String dbUsername;
+
+//     @Value("${spring.datasource.password}")
+//     private String dbPassword;
+
+//     @Value("${spring.datasource.driver-class-name}")
+//     private String dbDriverClassName;
+
+//     @Bean
+//     public Sql2o sql2o() {
+//         return new Sql2o(dbUrl, dbUsername, dbPassword);
+//     }
+// }
